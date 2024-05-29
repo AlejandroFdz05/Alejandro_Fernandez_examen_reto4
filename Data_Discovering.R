05a2e3a8cb483717e5022a2bde71fe40e8acebee
Alejandro Fernandez <alejandro.fernandezi@alumni.mondragon.edu>
2024-05-29 10:09:20 +0200

    data discovering acabado

--- Data_Discovering.R
+++ Data_Discovering.R
@@ -16,3 +16,14 @@ sd(datos$Value, na.rm = T)
 sd(datos$MDL, na.rm = T)
 sd(datos$RDL, na.rm = T)
 
+datos %>% 
+  group_by(Area) %>% 
+  summarise(media = mean(Value, na.rm = T))
+
+df <- datos %>% 
+  group_by(Area) %>% 
+  summarise(media = mean(Value, na.rm = T))  
+df$Area <- sort(df$Area, na.last = F)  #haciendo esto podemos obserbar que da la casualidad que ya estan ordenados
+
+df[c(5,20,33),]
+
